c0992307975bffdf089860bf20f18c0f6fde1c30
Tamer AbdulRadi <tamer@abdulradi.com>
2017-07-26 23:33:20 +0100

    type params

--- build.sbt
+++ build.sbt
@@ -1,7 +1,49 @@
-scalaVersion := "2.12.1"
+lazy val baseSettings: Seq[Setting[_]] = Seq(
+  scalaVersion       := "2.12.1",
+  scalaOrganization := "org.typelevel",
+  scalacOptions     ++= Seq(
+    "-deprecation",
+    "-encoding", "UTF-8",
+    "-feature",
+    "-language:higherKinds",
+    "-language:implicitConversions", "-language:existentials",
+    "-unchecked",
+    "-Xfatal-warnings",
+    "-Xlint",
+    "-Yno-adapted-args",
+    "-Ywarn-numeric-widen",
+    "-Ywarn-value-discard",
+    "-Xfuture",
+    "-Yliteral-types"
+  ),
+  resolvers += Resolver.sonatypeRepo("releases"),
 
-libraryDependencies ++= Seq(
-  "com.chuusai" %% "shapeless" % "2.3.2"
+  libraryDependencies ++= Seq(
+    "com.chuusai" %% "shapeless" % "2.3.2",
+    "org.scala-lang" % "scala-reflect" % "2.12.1",
+    "com.datastax.cassandra" % "cassandra-driver-core" % "3.0.0",
+    "com.lihaoyi" %% "fastparse" % "0.4.2",
+    "org.scalameta" %% "scalameta" % "1.6.0"
+  )
 )
 
-tutSettings
+lazy val `typelevel-cql` = project.in(file("."))
+  .settings(moduleName := "typelevel-cql")
+  .settings(baseSettings: _*)
+  .aggregate(core, slides)
+  .dependsOn(core, slides)
+
+lazy val core = project
+  .settings(moduleName := "typelevel-cql-core")
+  .settings(baseSettings: _*)
+
+
+lazy val slides = project
+  .settings(moduleName := "typelevel-cql-slides")
+  .settings(baseSettings: _*)
+  .settings(tutSettings: _*)
+  .settings(
+    tutSourceDirectory := baseDirectory.value / "tut",
+    tutTargetDirectory := baseDirectory.value / "tut-out",
+    watchSources ++= (tutSourceDirectory.value ** "*.html").get
+  ).dependsOn(core)
--- src/main/scala/typeparamcurry/Main.scala
+++ src/main/scala/typeparamcurry/Main.scala
@@ -2,127 +2,49 @@ package typeparamcurry
 
 import shapeless._
 
+trait DefaultValue[A] {
+  def value: A
+}
+object DefaultValue {
+  def instance[A](a: A) = new DefaultValue[A] { 
+      override def value = a 
+  }
+  implicit def optionsInstance[T] = instance[Option[T]](None)
 
-// def foo(a: Int)(b: String) = ???
-// val bar = foo(5) _
-// bar("Hello")
-
-
-// def foo(a: Int, b: String) = ???
-// val bar = foo(5, _: String)
-// bar("Hello")
-
-
-// def foo[A][B] = ???
-
-
-// def foo[A, B] = ???
-// val bar = foo[A, _]
-
-// def bar[B] = foo[Int, B]
-// bar[String]
-
-// def foo[A] = new {
-//     def apply[B] = ???
-// }
-// foo[Int][String]
-// val bar = foo[Int]
-// bar[String]
-
-object migration {
-    trait DefaultValue[A] {
-        def value: A
-    }
-    object DefaultValue {
-        def instance[A](a: A) = new DefaultValue[A] { 
-            override def value = a 
-        }
-        implicit def optionsInstance[T] = instance[Option[T]](None)
-
-        implicit val hNil = instance[HNil](HNil)
-        implicit def hCons[H, T <: HList](
-            implicit 
-            headDefault: DefaultValue[H],
-            tailDefault: DefaultValue[T]
-        ) = instance[H :: T](headDefault.value :: tailDefault.value)
-    }
-
-def migrate[
-  A, 
-  B, 
-  ARepr <: HList, 
-  BRepr <: HList,
-  Added <: HList
-](a: A, dummy: => B)(
-  implicit
-  aGen: Generic.Aux[A, ARepr],
-  bGen: Generic.Aux[B, BRepr],
-  diff: ops.hlist.Diff.Aux[BRepr, ARepr, Added],
-  default: DefaultValue[Added],
-  prepend: ops.hlist.Prepend.Aux[ARepr, Added, BRepr]
-): B = bGen.from(prepend(aGen.to(a), default.value))
-
-
-    // def migrate[B] = new {
-    //     def apply[
-    //         A,
-    //         ARepr <: HList, 
-    //         BRepr <: HList,
-    //         Added <: HList
-    //     ](a: A)(
-    //         implicit
-    //         aGen: Generic.Aux[A, ARepr],
-    //         bGen: Generic.Aux[B, BRepr],
-    //         diff: ops.hlist.Diff.Aux[BRepr, ARepr, Added],
-    //         default: DefaultValue[Added],
-    //         prepend: ops.hlist.Prepend.Aux[ARepr, Added, BRepr]
-    //     ): B  = bGen.from(prepend(aGen.to(a), default.value))
-    // }
-
-    // trait MyMigration[A, B] extends (A => B)
-    // object MyMigration {
-    //     implicit def instance[
-    //         A,
-    //         B,
-    //         ARepr <: HList, 
-    //         BRepr <: HList,
-    //         Added <: HList
-    //     ](
-    //         implicit
-    //         aGen: Generic.Aux[A, ARepr],
-    //         bGen: Generic.Aux[B, BRepr],
-    //         diff: ops.hlist.Diff.Aux[BRepr, ARepr, Added],
-    //         default: DefaultValue[Added],
-    //         prepend: ops.hlist.Prepend.Aux[ARepr, Added, BRepr]
-    //     )  = new MyMigration[A, B] {
-    //         override def apply(a: A): B = 
-    //           bGen.from(prepend(aGen.to(a), default.value))
-    //     }
-    // }
-
-    // def migrate[A, B](value: A)(implicit migrate: MyMigration[A, B]): B = 
-    //     migrate(value)
-    
+  implicit val hNil = instance[HNil](HNil)
+  implicit def hCons[H, T <: HList](
+      implicit 
+      headDefault: DefaultValue[H],
+      tailDefault: DefaultValue[T]
+  ) = instance[H :: T](headDefault.value :: tailDefault.value)
 }
 
-// package protocol.v1
-// case class Employee(name: String)
 
-object model {
-    object v1 {
-        case class Employee(name: String)
-    }
-
-    object v2 {
-        case class Employee(name: String, salary: Option[Int])
-    }
+object v1 {
+  case class Employee(name: String)
 }
+
+object v2 {
+  case class Employee(name: String, salary: Option[Int])
+}
+
 
 object Main extends App {
-    import model._
-    import migration._
 
+  def migrate[
+    A, 
+    B, 
+    ARepr <: HList, 
+    BRepr <: HList,
+    Added <: HList
+  ](a: A)(
+    implicit
+    aGen: Generic.Aux[A, ARepr],
+    bGen: Generic.Aux[B, BRepr],
+    diff: ops.hlist.Diff.Aux[BRepr, ARepr, Added],
+    default: DefaultValue[Added],
+    prepend: ops.hlist.Prepend.Aux[ARepr, Added, BRepr]
+  ): B = bGen.from(prepend(aGen.to(a), default.value))
 
-    lazy val dummy: v2.Employee = ???
-    println(migrate(new v1.Employee("X"), dummy))
+  // println(migrate(new v1.Employee("foo"), dummy))
 }
